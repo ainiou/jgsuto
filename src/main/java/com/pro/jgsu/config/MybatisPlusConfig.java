cb6a73d77b95f25de38d86e97040787ff49cf2e5
xwh <123@139.com>
2022-08-01 23:28:07 +0800

    完成了员工的分页展示、模糊查询、状态禁用 配置了静态资源拦截器

---
+++ src/main/java/com/pro/jgsu/config/MybatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.pro.jgsu.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置MP分页插件
+ * @Author XWH
+ * @Time 2022/8/1 下午 21:23
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+
+}
---
+++ src/main/java/com/pro/jgsu/config/WebMvcConfig.java
@@ -0,0 +1,49 @@
+package com.pro.jgsu.config;
+
+import com.pro.jgsu.common.JacksonObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
+
+/**
+ * @Author XWH
+ * @Time 2022/8/1 下午 23:01
+ */
+
+@Slf4j
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+
+    /**
+     * 设置静态资源映射
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始进行静态资源映射...");
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+
+
+    /**
+     * 扩展SpringMVC消息转换器,使Long 型变量转换为String类型后序列化到JSON中传递给前端，解决JS处理精度问题
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+
+        //创建消息转换器对象
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        //设置转换器对象将JAVA对象序列化为JSON
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        //将转换器追加到mvc框架的转换器集合的首位
+        converters.add(0,messageConverter);
+    }
+}
