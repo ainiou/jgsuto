4f0211493bdef24cae452baa6286067a100e0719
xwh <123@139.com>
2022-11-19 12:26:31 +0800

    完成了 redis 缓存验证码，弃用之前存储在 session 中的方案，改用 redis 存储，key 为 邮箱号，value 为随机生成的四位验证码，并设置过期时间为五分钟

--- src/main/java/com/pro/jgsu/controller/UserController.java
+++ src/main/java/com/pro/jgsu/controller/UserController.java
@@ -9,6 +9,7 @@ import com.pro.jgsu.utils.ValidateCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
@@ -23,6 +25,9 @@ import java.util.Map;
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     /**
      * 获取邮箱验证码
@@ -43,9 +48,14 @@ public class UserController {
         //发送邮件
         //MailUtils.sendMail(phone,"【井冈山外卖】您正在登录井冈山外卖，验证码为" + code + "有效期五分钟,如非本人操作，请忽略本邮件！",
         //        "井冈山外卖验证码");
-        //将校验码存入session中
-        session.setAttribute("code",code);
-        log.info("code={}",code);
+        //将校验码存入session中(已弃用，改用redis缓存)
+        //session.setAttribute("code",code);
+
+        //将生成的验证码存入 redis 缓存，并设置有效期为五分钟
+        redisTemplate.opsForValue().set(phone,code,5, TimeUnit.MINUTES);
+        //log.info("code={}",code);
+        String codeInRedis = redisTemplate.opsForValue().get(phone).toString();
+        log.info("redis 中的验证码{}",codeInRedis);
 
         return R.success("已向您的邮箱发送验证码，请注意查收！");
     }
@@ -56,8 +66,12 @@ public class UserController {
         //获取手机号和验证码
         String phone = map.get("phone").toString();
         String code = map.get("code").toString();
-        //获取session中保存的验证码
-        String codeInSession = session.getAttribute("code").toString();
+        //获取session中保存的验证码(已弃用，改用 redis 缓存)
+        //String codeInSession = session.getAttribute("code").toString();
+
+        //从 redis 中获取验证码
+        String codeInSession = redisTemplate.opsForValue().get(phone).toString();
+
         //将两个code进行对比
         if(codeInSession != null && codeInSession.equals(code)){
             //相同则登录成功
@@ -71,8 +85,10 @@ public class UserController {
                 user.setPhone(phone);
                 userService.save(user);
             }
-            //将用户信息存入session
+            //用户登录成功，将用户信息存入session
             session.setAttribute("user",user.getId());
+            //删除 redis 中缓存的验证码
+            redisTemplate.delete(phone);
             return R.success(user);
         }
 
